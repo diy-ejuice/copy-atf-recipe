79cb476f7ecf91f36626ec97adedcac4198513a9
ayan4m1 <andrew@bulletlogic.com>
2021-10-14 16:00:04 -0700

    refactor script injection to separate const

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,6 @@
-chrome.browserAction.onClicked.addListener(() => {
+const copyToClipboard = () =>
   chrome.tabs.executeScript({ file: "src/js/jquery.min.js" }, () => {
     chrome.tabs.executeScript({ file: "src/js/markdown.js" });
   });
-});
+
+chrome.browserAction.onClicked.addListener(copyToClipboard);
