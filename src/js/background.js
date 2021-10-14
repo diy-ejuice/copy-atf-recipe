08acd69546866fe5edae03362c7a91362d88f01c
ayan4m1 <andrew@bulletlogic.com>
2021-10-14 16:43:03 -0700

    provide support for firefox with v2 manifest and chrome with v3 manifest

--- src/js/background.js
+++ src/js/background.js
@@ -1,11 +1,27 @@
-const copyToClipboard = (tab) =>
-  chrome.scripting.executeScript(
-    { target: { tabId: tab.id }, files: ["src/js/jquery.min.js"] },
-    () =>
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["src/js/markdown.js"],
-      })
-  );
+const isFirefox = () =>
+  navigator.userAgent.toLowerCase().indexOf("firefox") !== -1;
 
-chrome.action.onClicked.addListener(copyToClipboard);
+const copyToClipboard = (tab) => {
+  if (isFirefox()) {
+    browser.tabs.executeScript({ file: "src/js/jquery.min.js" }, () =>
+      browser.tabs.executeScript({
+        file: "src/js/markdown.js",
+      })
+    );
+  } else {
+    chrome.scripting.executeScript(
+      { target: { tabId: tab.id }, files: ["src/js/jquery.min.js"] },
+      () =>
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ["src/js/markdown.js"],
+        })
+    );
+  }
+};
+
+if (isFirefox()) {
+  chrome.browserAction.onClicked.addListener(copyToClipboard);
+} else {
+  chrome.action.onClicked.addListener(copyToClipboard);
+}
